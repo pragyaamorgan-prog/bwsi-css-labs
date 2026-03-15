88870d1268f772a3026c0ed06e650122ee9167cf
pragyaamorgan-prog <pragya.amorgan@gmail.com>
2026-03-15 14:01:48 -0400

    Update lab_1a.py
    added intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Pragya Khandelwal" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("I am a tenth grader from NYC")
 
 if __name__ == "__main__":
     main()
